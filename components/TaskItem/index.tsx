198787eac4ece9fe46728ab1d35abf83c29e9f96
fomes <fgomesdeluna@gmail.com>
2024-03-09 12:00:03 -0300

    creates task item component

---
+++ components/TaskItem/index.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { BsCheckCircleFill } from "react-icons/bs";
+import { FaRegCircle, FaTrash } from "react-icons/fa";
+
+interface TaskItemProps {
+  id: string;
+  desc: string;
+  done: boolean;
+  handleDeleteTask: (id: string) => void;
+  handleUpdateTaskState: (id: string) => void;
+}
+
+export default async function TaskItem({
+  id,
+  desc,
+  done,
+  handleDeleteTask,
+  handleUpdateTaskState,
+}: TaskItemProps) {
+  let styleClass = done ? "styles.taskCreated" : "styles.taskFinished";
+
+  return (
+    <div className="bg-tm-gray-500 rounded-lg h-[4.5rem] flex items-center justify-between py-0 px-5">
+      <span className={styleClass}>
+        {!done ? (
+          <FaRegCircle
+            onClick={() => handleUpdateTaskState(id)}
+            className="text-tm-b-100 hover:text-tm-blue-200"
+          />
+        ) : (
+          <BsCheckCircleFill
+            onClick={() => handleUpdateTaskState(id)}
+            className="text-tm-purple-200 hover:text-tm-purple-100"
+          />
+        )}
+      </span>
+      <p className={`${done && "line-through"}`}>{desc}</p>
+      <span onClick={() => handleDeleteTask(id)}>
+        <FaTrash className="text-tm-gray-300 hover:text-tm-red-200" />
+      </span>
+    </div>
+  );
+}
